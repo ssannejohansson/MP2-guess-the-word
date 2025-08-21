4f3f6789e2878017396bcd35bbb126c1e979e069
ssannejohansson <sannej0hansson@hotmail.com>
2025-08-21 10:50:35 +0200

    Store user input in variable, show in alert to test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,13 @@ const gameContainer = document.getElementById("game-container");
 const backBtn = document.getElementById("back-btn");
 
 // Heading elements 
+const headingContainer = document.getElementById("heading-container");
 const heading = document.getElementById("heading");
 const headingSmall = document.getElementById("heading-small");
-const headingContainer = document.getElementById("heading-container");
-
 // Input elements 
 const inputContainer = document.getElementById("input-container");
-const inputInfo = document.getElementById("input-info");
 const inputForm = document.getElementById("input-form");
+const continueToDiff = document.getElementById("continue-btn");
 
 // Info elements
 const infoContainer = document.getElementById("info-container");
@@ -18,3 +17,10 @@ const infoText = document.getElementById("info-text");
 
 // Start game
 const startGame = document.getElementById("start-btn");
+
+
+// Store username from input field
+function userName() {
+    const input = document.getElementById("userInput").value 
+    alert(input)
+}
