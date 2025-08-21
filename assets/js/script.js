1910b1f31d67739e12eedf7f80fe38a30c8e5cd6
ssannejohansson <sannej0hansson@hotmail.com>
2025-08-21 09:46:07 +0200

    Add DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// DOM 
+const gameContainer = document.getElementById("game-container");
+const backBtn = document.getElementById("back-btn");
+
+// Heading elements 
+const heading = document.getElementById("heading");
+const headingSmall = document.getElementById("heading-small");
+const headingContainer = document.getElementById("heading-container");
+
+// Input elements 
+const inputContainer = document.getElementById("input-container");
+const inputInfo = document.getElementById("input-info");
+const inputForm = document.getElementById("input-form");
+
+// Info elements
+const infoContainer = document.getElementById("info-container");
+const infoText = document.getElementById("info-text");
+
+// Start game
+const startGame = document.getElementById("start-btn");
