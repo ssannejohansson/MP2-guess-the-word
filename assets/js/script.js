d97bb361261739e762ebc279610100310ee84b1a
ssannejohansson <sannej0hansson@hotmail.com>
2025-08-22 09:34:22 +0200

    Add temporary word array, will change later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,21 @@
-
-// DOM 
-const gameContainer = document.getElementById("game-container");
-const backBtn = document.getElementById("back-btn");
-
-// Heading elements 
-
-// Input elements 
-const inputContainer = document.getElementById("input-container");
-const inputForm = document.getElementById("input-form");
-const continueToDiff = document.getElementById("continue-btn");
-
-// Info elements
-const infoContainer = document.getElementById("info-container");
-const infoText = document.getElementById("info-text");
-
-// Start game
-const startGame = document.getElementById("start-btn");
+// BEFORE GAME STARTS 
 
 // Selection dropdown variable 
-let selectDifficulty = '<div class="d-flex flex-column justify-content-center align-items-center">' + 
-                            '<p>Please select difficulty</p>' +
-                                '<form class="mb-3">' + 
-                                    '<select class="form-select mt-2" aria-label="Select difficulty">' + 
-                                    '<option selected value="1">Easy</option>' +
-                                    '<option value="2">Medium</option>' +
-                                    '<option value="3">Hard</option>' +
-                                    '</select>' +
-                                '</form>' + 
-                        '</div>'; 
+let selectDifficulty = '<div class="d-flex flex-column justify-content-center align-items-center">' +
+    '<p>Please select difficulty</p>' +
+    '<form class="mb-3">' +
+    '<select class="form-select mt-2" aria-label="Select difficulty">' +
+    '<option selected value="1">Easy</option>' +
+    '<option value="2">Medium</option>' +
+    '<option value="3">Hard</option>' +
+    '</select>' +
+    '</form>' +
+    '</div>';
 
-let instructions = '<div class="d-flex flex-column justify-content-center align-items-center">' + 
-                    '<p>Instructions: </p>' + 
-                    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores amet et eum, explicabo minima quam praesentium laudantium voluptate nemo exercitationem possimus veritatis, libero quibusdam illo? Dignissimos necessitatibus qui unde.</p>' +
-                    '</div>';
+let instructions = '<div class="d-flex flex-column justify-content-center align-items-center">' +
+    '<p>Instructions: </p>' +
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores amet et eum, explicabo minima quam praesentium laudantium voluptate nemo exercitationem possimus veritatis, libero quibusdam illo? Dignissimos necessitatibus qui unde.</p>' +
+    '</div>';
 
 let startBtn = '<a href="game.html" type="button" class="btn btn-primary mt-2" id="start-btn">Start Game</a>';
 
@@ -41,14 +24,34 @@ let startBtn = '<a href="game.html" type="button" class="btn btn-primary mt-2" i
 document.getElementById("continue-btn").addEventListener("click", e => diffPage(e));
 
 function diffPage(e) {
-const input = document.getElementById("userInput").value; // Store input in a variable
-$("#heading-small").remove();
-$("#heading").text(`Hello ${input}!`).css("text-transform", "capitalize");
-$("#userInput").css("text-transform", "capitalize");
-$("#input-container").children().remove();
-$("#input-container").append(selectDifficulty).append(instructions).append(startBtn);
+    const input = document.getElementById("userInput").value; // Store input in a variable
+    $("#heading-small").remove();
+    $("#heading").text(`Hello ${input}!`).css("text-transform", "capitalize");
+    $("#userInput").css("text-transform", "capitalize");
+    $("#input-container").children().remove();
+    $("#input-container").append(selectDifficulty).append(instructions).append(startBtn);
 }
 
+// GAME FUNCTIONALITY
 
-
-
+const wordList = [{
+        word: "hello",
+        hint: "greeting"
+    },
+    {
+        word: "cat",
+        hint: "says meow"
+    },
+    {
+        word: "dog",
+        hint: "says woff"
+    },
+    {
+        word: "water",
+        hint: "something to drink"
+    },
+    {
+        word: "home",
+        hint: "where you live"
+    },
+]
