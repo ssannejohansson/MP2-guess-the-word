bdbb32f5fa05456b6ec597466395855a65b1a27a
ssannejohansson <sannej0hansson@hotmail.com>
2025-08-25 10:43:54 +0200

    Style game interface

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // BEFORE GAME STARTS 
 
 // Selection dropdown variable 
-let selectDifficulty = '<div class="d-flex flex-column justify-content-center align-items-center ps-2 pe-2">' +
+let selectDifficulty = '<div class="d-flex flex-column justify-content-center align-items-center p-2">' +
     '<p class="mb-0">Choose your difficulty</p>' +
     '<div class="d-flex gap-2 mb-2 mt-2" id="button-container">' + 
     '<button class="btn diff-btn" id="easy" onclick="changeDiff(1)">Easy</button>' +
@@ -10,10 +10,11 @@ let selectDifficulty = '<div class="d-flex flex-column justify-content-center al
     '</div>';
 
     // Instructions variable
-let instructions = '<div class="d-flex flex-column justify-content-center align-items-center p-2">' +
-    '<p>Click the button below to generate a random word. Use your keyboard to guess a letter.</p>' +
+let instructions = '<div class="d-flex flex-column justify-content-center align-items-center p-2 mt-3 mb-3 text-center">' +
+    '<p>Click the button below to generate a random word.</p>' +
+    '<p>Use your keyboard to guess a letter.</p>' +
     '<p>When your guess is correct the letter will automaticlly will be inserted to the word.</p>' +
-    '<button type="button" class="btn custom-btn mt-2" id="start-btn">Start Game</button>' +
+    '<button type="button" class="btn custom-btn mt-3" id="start-btn">Start Game</button>' +
     '</div>';
 
 
@@ -36,24 +37,24 @@ function changeDiff (difficulty) {
     if (difficulty === 1) {
         $("#easy").on("click").toggleClass("active-btn"); 
         $("#medium, #hard").on("click").removeClass("active-btn");
-        wordList = easyWords.slice();
+        wordList.push(...easyWords);
     } else if (difficulty === 2) {
           $("#medium").on("click").toggleClass("active-btn"); 
           $("#easy, #hard").on("click").removeClass("active-btn");
-        wordList = mediumWords.slice();
+        wordList.push(...hardWords);
     } else if (difficulty === 3) { 
           $("#hard").on("click").toggleClass("active-btn"); 
           $("#medium, #easy").on("click").removeClass("active-btn");
-        wordList = hardWords.slice();
+        wordList.push(...hardWords);
         
     }
 };
 
 // Game info variable
-let gameInfo = '<div class="d-flex flex-column p-2" id="game-info">' +
-                '<h6>Hint:</h6><p id="hint"></p>' +
+let gameInfo = '<div class="container d-flex flex-column gap-2 p-2 mt-3 text-center" id="game-info">' +
+                '<h6>Hint</h6>' +
                 '<p id="hint"></p>' +
-                '<h6>Wrong letters:</h6>' +
+                '<h6>Wrong letters</h6>' +
                 '<p id="wrong-letters"></p>' +
                 '<h6>Remaining guesses:</h6>' +
                 '<p id="rem-guess"></p>' +
@@ -65,7 +66,7 @@ let resetBtn = '<button type="button" class="btn custom-btn mt-2" id="reset-btn"
 // Start game page
 function startGame() {
     randomWord();
-$("#heading").text("Guess the word!").css("text-transform", "uppercase");
+$("#heading").text("Good luck!").addClass("mb-4");
 $("#input-container").children().remove();
 $("#letter-container").removeClass("d-none");
 $("#typing-input").removeClass("d-none");
@@ -81,85 +82,85 @@ let wordList =  [];
 
 const easyWords = [{
     word: "apple",
-    hint: "a fruit that keeps the doctor away"
+    hint: "A fruit that keeps the doctor away"
 }, 
 {
     word: "pizza",
-    hint: "a cheesy favourite from Italy"
+    hint: "A cheesy favourite from Italy"
 }, 
 {
     word: "beach",
-    hint: "sandy spot by the ocean"
+    hint: "A sandy spot by the ocean"
 }, 
 {
     word: "sheep",
-    hint: "woolly animal that goes 'baa'"
+    hint: "A woolly animal that goes 'baa'"
 }, 
 {
     word: "cloud",
-    hint: "fluffy thing in the sky or used in tech"
+    hint: "A fluffy thing in the sky or used in tech"
 }, 
 {
     word: "ghost",
-    hint: "spooky and white"
+    hint: "Something spooky and white"
 }, 
 {
     word: "light",
-    hint: "opposite of dark"
+    hint: "The opposite of dark"
 }];
 
 const mediumWords = [{
     word: "elephant",
-    hint: "largest land animal"
+    hint: "The largest land animal"
 },
 {
     word: "computer",
-    hint: "machine used for work or games"
+    hint: "A machine used for work or games"
 },
 {
     word: "backpack",
-    hint: "something you carry on your back"
+    hint: "Something you carry on your back"
 },
 {
     word: "airplane",
-    hint: "carries passengers in the sky"
+    hint: "Something that carries passengers in the sky"
 },
 {
     word: "umbrella",
-    hint: "protects you from rain"
+    hint: "Something that protects you from rain"
 },
 {
     word: "fireworks",
-    hint: "loud and colorful show in the sky"
+    hint: "A loud and colorful show in the sky"
 }];
 
 const hardWords = [{
     word: "brainstorming",
-    hint: "creative idea generation process"
+    hint: "A creative idea generation process"
 },
 {
     word: "relationship",
-    hint: "a connection between people"
+    hint: "A connection between people"
 },
 {
     word: "investigation",
-    hint: "careful search or examination"
+    hint: "A careful search or examination"
 },
 {
-    word: "elephants",
-    hint: "lives in africa"
+    word: "journalist",
+    hint: "Someone who reports the news for TV, newspaper or websites"
 },
 {
-    word: "elephants",
-    hint: "lives in africa"
+    word: "wilderness",
+    hint: "Untamed nature, often far from cities"
 },
 {
-    word: "elephants",
-    hint: "lives in africa"
+    word: "lighthouse",
+    hint: "A tall structure with beacon to guide ships"
 },
 {
-    word: "elephants",
-    hint: "lives in africa"
+    word: "smartphone",
+    hint: "A device that lets you scroll all day"
 }];
 
 
@@ -184,6 +185,7 @@ const guessLeft = document.querySelector("#rem-guess");
 
 
 function randomWord() {
+    changeDiff();
     let randomObject = wordList[Math.floor(Math.random() * wordList.length)]; // Gets a random object from wordList array
     word = randomObject.word; // Fetches a random word 
     hints = randomObject.hint; // Fetches a random hint
@@ -206,11 +208,11 @@ function randomWord() {
 
 }
 
-randomWord();
 
 
 
-function gameFunc(e) {
+
+function initGame(e) {
     let key = e.target.value;
     if(key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key}`) && !corrects.includes(key)) {
          console.log(key);   
@@ -228,7 +230,7 @@ function gameFunc(e) {
     }
 
 
-    $("#wrong-letters").text(incorrects);
+    $("#wrong-letters").text(incorrects).css("text-transform", "uppercase");
     $("#rem-guess").text(maxGuess);
  }
     typingInput.value = ""; // Empty input-field after typing a letter
